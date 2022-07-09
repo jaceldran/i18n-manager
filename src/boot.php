01b234ed1137652943b33173936582bbba60cae0
Juan Ángel Celdrán <jaceldran@gmail.com>
2022-07-09 09:57:12 +0200

    Add .tmp folder

--- src/boot.php
+++ src/boot.php
@@ -11,7 +11,7 @@ define ('BASE_PATH', $base_path);
 define ('PATH_DATA_LANGS', "$base_path/data/i18n/langs.php");
 define ('PATH_DATA_ENTRIES', "$base_path/data/i18n/entries.php");
 define ('BLADE_VIEWS', "$base_path/src/Views");
-define ('BLADE_COMPILED', "$base_path/data/blade");
+define ('BLADE_COMPILED', "$base_path/.tmp/blade");
 
 /** view engine */
 Flight::register('view', BladeOne::class, [
