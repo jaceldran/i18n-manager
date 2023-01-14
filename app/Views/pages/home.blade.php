95feba34022a9824f3156a8e02a905b4540cd3cb
Juan Ángel Celdrán <jaceldran@gmail.com>
2023-01-14 21:56:18 +0100

    Change home screen

--- app/Views/pages/home.blade.php
+++ app/Views/pages/home.blade.php
@@ -8,10 +8,8 @@
     <div class="h-screen flex justify-center items-center">
         <div id="intro" class="cursor-default fixed left-[25%] right-[25%]">
             <div class="container mx-auto w-auto">
-                <span style="font-family:'Fjalla One',sans-serif"
-                    class="tracking-tighter font-ser font-bold text-4xl text-amber-600">i18n</span>
-
-                <div class="text-black font-extrabold text-9xl -translate-y-8">
+                <span class="tracking-tighter font-ser font-bold text-4xl text-amber-600  font-fjalla-one">i18n</span>
+                <div class="text-black font-extrabold text-9xl -translate-x-1 -translate-y-12" style="font-family:sans-serif">
                     <span class="stroke">man<span id="button-play">a</span>ger</span>
                 </div>
             </div>
